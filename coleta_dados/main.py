b76efca9c1c32f6bc1ee86985ee14d14851e6c6b
Heuller <heullergit@gmail.com>
2020-07-14 15:31:23 -0300

    captura de link de jogos de campeonato nacional e copas

--- coleta_dados/main.py
+++ coleta_dados/main.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-url_campeonato = 'https://www.academiadasapostasbrasil.com/stats/competition/espanha/7'
+#Campeonato (FAZER UM FOR PARA PERCORRER OS CAMPEONATOS)
+#criar flag pra campeonato nacional e copa
+#criar variavel para percorrer um campeonato até quando existir linha do tempo do jogo, se caso for encontrado um jogo sem linha do tempo, pular para proximo campeonato
+url_campeonato = 'https://www.academiadasapostasbrasil.com/stats/competition/espanha/138'
 r = requests.get(url_campeonato)
 
 soup = BeautifulSoup(r.content, 'html.parser')
@@ -18,7 +21,7 @@ for codigo in codigo_temporadas:
     link = url_campeonato + '/' + codigo
     link_alternar_temporadas.append(link)
 
-#Primeira temporada (FAZER UM FOR PARA PERCORRER AS TEMPORADAS)
+#Temporada (FAZER UM FOR PARA PERCORRER AS TEMPORADAS)
 url_temporada = link_alternar_temporadas[0]
 r = requests.get(url_temporada)
 soup = BeautifulSoup(r.content, 'html.parser')
@@ -44,58 +47,24 @@ link_paginas_jogos.append(link_ver_jogos)
 for i in range(2, qnt_paginas + 1, 1):
     url = link_ver_jogos + '/page/' + str(i)
     link_paginas_jogos.append(url)
-    
 
-
-
-
-
-
-
-
-
-
-'''
-
-rodada_dados = soup.find('tr', class_="competition-tr-title")
-
-#Captura o link para alternar entre as rodadas
-link_alternar_rodada = rodada_dados.find('td', id="week-gr").find(class_="group-url")['value']
-
-#Captura a rodada atual se o campeonato estiver em andamento, ou a última rodada se o campeonato já estiver encerrado
-ultima_rodada = int(rodada_dados.find('td', id="week-gr").span.text)
-
-#Links das rodadas do ano
-link_rodadas_do_ano = []
-for i in range(ultima_rodada, 0, -1):
-    link_rodada = link_alternar_rodada + str(i)
-    link_rodadas_do_ano.append(link_rodada)
-
-#Rodada (FAZER UM FOR PARA PERCORRER AS RODADAS)
-url_rodada = link_rodadas_do_ano[1]
-r = requests.get(url_rodada)
-soup = BeautifulSoup(r.content, 'html.parser')
-
-jogos = soup.find('table', class_="competition-rounds").find_all('tr')
 link_jogos = []
-
-
-##Ao verificar se existe dados do jogo pra capturar, verificar se existe ficha, verificar se existe se jogo foi adiado (pois nesses jogos existe ficha), verificar se jogo está em andamento
-for jogo in jogos:
-    ficha = jogo.find(class_="aa-icon-player") #icone de ficha do jogo
-
-    placar_info = jogo.find_all('td')[3]
-    placar = placar_info.text.strip() #placar do jogo
-    em_andamento = placar_info['class'] #vetor com as classes de placar
+for pagina in link_paginas_jogos:
+    r = requests.get(pagina)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    jogos = soup.find('table', class_="competition-rounds").find_all('tr')
     
-    if((ficha) and (placar != 'Postponed') and ('gameinlive' not in em_andamento)): #existe ficha do jogo, jogo não foi adiado, jogo não está em andamento
-        link = jogo.find_all('td')[6].find('a')['href'] #posição 6 pois sempre é o sexta coluna(td) dentro da linha(tr)
-        link_jogos.append(link)
+    for jogo in jogos:
+        ficha = jogo.find(class_="aa-icon-player") #icone de ficha do jogo
+        if(ficha):
+            placar_info = jogo.find_all('td')[3] #informacoes do placar
+            placar = placar_info.text.strip() #placar do jogo
+            em_andamento = placar_info['class'] #vetor com as classes de placar
 
-#Jogo (FAZER UM FOR PARA PERCORRER OS JOGOS)
-jogo = link_jogos[0]
-'''
-
+            if((placar != 'Postponed') and ('gameinlive' not in em_andamento)): #existe ficha do jogo, jogo não foi adiado, jogo não está em andamento
+                link = ficha.parent #o pai do elemento ficha, possui o link do jogo
+                link = link['href']
+                link_jogos.append(link)
 
 
 
